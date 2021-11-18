a466d792d1239df2175ed6e80df60b5652c80f27
Gonçalves, Marcelo <tchelojg@gmail.com>
2021-11-18 19:20:11 -0300

    Adding function which returns a list of downloaded movies

---
+++ list-downloaded-movies.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python3
+# A function that returns all movies or folders containing movies of a given directory
+
+import os
+import re
+
+
+def listDownloadedMovies(path):
+
+    foldersList = []
+    moviesList = []
+    count = 0
+
+    os.chdir(targetFolder)
+    foldersContent = os.listdir(targetFolder)
+    extensionsRegex = re.compile(r'.*\.avi|.*\.mkv|.*\.mp4')
+
+    for item in range(len(foldersContent)):
+        if os.path.isdir(foldersContent[item]):
+            foldersList.append(foldersContent[item])
+
+        matchObjetc = extensionsRegex.search(foldersContent[item])
+
+        if matchObjetc != None:
+            moviesList.append(matchObjetc.group())
+
+    while count < len(foldersList):
+        for foldername, _, filenames in os.walk(foldersList[count]):
+            for filename in range(len(filenames)):
+                if filenames[filename].endswith('.part'):
+                    continue
+
+                matchObjetc = extensionsRegex.search(filenames[filename])
+
+                if matchObjetc != None:
+                    moviesList.append(foldername)
+
+        count += 1
+    return moviesList
+
+
+targetFolder = '/media/mjgoncalves/hd1/development/python/donwloads'
+movies = listDownloadedMovies(targetFolder)
+print(movies)
