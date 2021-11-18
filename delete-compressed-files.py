8fcb4a4bd9947dde4f9852086f061d6781ed23d8
Gonçalves, Marcelo <tchelojg@gmail.com>
2021-11-18 19:29:56 -0300

    function that deletes compressed files

---
+++ delete-compressed-files.py
@@ -0,0 +1,33 @@
+# Functions which searches for compressed files and deletes them
+
+import re
+import os
+
+
+def findCompressedFiles(path):
+
+    filesRegex = re.compile(r'.*\.zip|.*\.rar|.*\.tgz|.*\.tar\.gz')
+    compressedFiles = []
+    allFiles = os.listdir(path)
+
+    for filename in range(len(allFiles)):
+
+        matchObject = filesRegex.search(allFiles[filename])
+
+        if matchObject != None:
+            compressedFiles.append(matchObject.group())
+
+    return compressedFiles
+
+
+def deleteCompressedFiles(listOfFiles):
+    for filename in range(len(listOfFiles)):
+        if os.path.exists(listOfFiles[filename]):
+            os.remove(listOfFiles[filename])
+
+
+files = findCompressedFiles(
+    '/media/mjgoncalves/hd1/development/python/teste')
+
+print(files)
+deleteCompressedFiles(files)
